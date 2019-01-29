d17ec2e21c2c7962a56df38c5ce2d6f710e66079
Rob Cheseldine <robert.cheseldine@gmail.com>
2019-01-28 22:29:24 -0500

    updated with material ui and error handling

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
 import {DatePicker} from './components/datepicker/datepicker'
-import {ImageOut} from './components/imageout/imageout'
-import {Input} from 'react-materialize';
+import {ImageOut,} from './components/imageout/imageout'
+import {MediaBox, Card, CardTitle, Col} from 'react-materialize';
 import './App.css';
 import axios from 'axios';
 
 class App extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
       value: "",
-      images: ""
+      images: "",
+      isLoading: true,
+
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -21,19 +24,33 @@ class App extends Component {
     this.setState({ value: event.target.value });
   }
 
+
   handleSubmit(event) {
-
-    let dateSelected = this.state.value;   
-
+    let dateSelected = this.state.value; 
     axios
       .get(
         "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=" +
           dateSelected +
           "&api_key=ey4JvDbo9fpSUq0gD528KBxEIAJ4E2WIAr4cJBTp"
       )
-      .then(response =>
-        this.setState({ images: response.data.photos[1].img_src })
-      );
+      .then(response =>       
+        this.setState({ 
+          images: response.data.photos[1].img_src, 
+          isLoading: false 
+        })
+      )
+      .catch(error => {
+        if (error.response) {
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        } else if (error.request) {
+          console.log(error.request);
+        } else {
+          console.log('Error', error.message);
+        }
+          console.log(error.config);
+      });
     event.preventDefault();
   }
 
@@ -41,30 +58,40 @@ class App extends Component {
     console.log(this.state.value);
     return (
       
-      <div>
-        
+      <div>        
           <form className="earthdate" onSubmit={this.handleSubmit}>
-            <label>
-              Date:
-              {/* <Input name='on' type='date' value={this.state.value} onChange={this.handleChange} /> */}
-              <input
+          Date:              
+               <input
                 type="date"
                 value={this.state.value}
                 onChange={this.handleChange}
-              />
-            </label>
+              />            
             <input type="submit" value="Submit" />
           </form>
         
         <hr />
         
-          <img
-            className={this.state.images ? "yesImg" : "noImg"}
-            src={this.state.images}
-          />
+          <div className='imageDisplay'> {this.state.isLoading?
+           <Card 
+            className='small' 
+            header={
+              <CardTitle image='http://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/01504/opgs/edr/fcam/FLB_531027377EDR_F0590000FHAZ00337M_.JPG'>
+              View Mars!
+              </CardTitle>
+            }>
+            Test out by selecting a date from above!
+          </Card>
+          : 
+          <MediaBox 
+          src={this.state.images} 
+          caption={this.state.value} 
+          width="350"/>}
         
-      </div>
-    );
+        </div>
+        
+    </div>);
+
+    
   }
 }
 
